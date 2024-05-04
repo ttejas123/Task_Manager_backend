1ca2edcbe563dd49e6dca98aa7e5a3264f07bd3a
ttejas123 <tthakare73@gmail.com>
2024-05-04 20:42:12 +0530

    added new things in ignore file

---
+++ src/main/java/com/tejas/googleauth/config/MailConfiguration.java
@@ -0,0 +1,10 @@
+package com.tejas.googleauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MailConfiguration {
+
+    
+}
